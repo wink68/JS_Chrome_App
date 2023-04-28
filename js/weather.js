6a1332a4e31150d8e47e032c2c75fa6bfb8ef4a7
김창민 <jinttack@naver.com>
2023-04-29 00:07:20 +0900

    feat: 날씨 기능 추가

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "f106a472745a0b328406fea696f4bd2b";
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(res => res.json()).then(data => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `날씨: ${data.weather[0].main} / 온도: ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+
