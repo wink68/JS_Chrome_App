2fc0ce45640e24a38ae68a53da0a63e937f5b985
김창민 <jinttack@naver.com>
2023-05-02 17:02:20 +0900

    style: 날씨 사이드바

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,7 @@
 const logOutBtn = document.querySelector("#logOut");
 
 function handleHome() {
+    alert("To Do List가 리셋됩니다");
     window.localStorage.removeItem("username");
     window.localStorage.removeItem("toDos");
     location.reload();
--- js/weather.js
+++ js/weather.js
@@ -5,9 +5,11 @@ function onGeoSuccess(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(res => res.json()).then(data => {
-      const weather = document.querySelector(".weather span:first-child");
-      const city = document.querySelector(".weather span:last-child");
-      weather.innerText = `날씨: ${data.weather[0].main} / 온도: ${data.main.temp}`;
+      const weather = document.querySelector(".weather span:nth-child(1)");
+      const temperature = document.querySelector(".weather span:nth-child(2)")
+      const city = document.querySelector(".weather span:nth-child(3)");
+      weather.innerText = `날씨: ${data.weather[0].main}`;
+      temperature.innerText = `온도: ${data.main.temp}`;
       city.innerText = data.name;
     });
 }
