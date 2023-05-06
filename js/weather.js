14ba61e51e80555f136358a1f2ad2884121cf71d
김창민 <jinttack@naver.com>
2023-05-06 12:49:24 +0900

    style: 날씨

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,20 @@ function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(res => res.json()).then(data => {
-      const weather = document.querySelector(".weather span:nth-child(1)");
-      const temperature = document.querySelector(".weather span:nth-child(2)")
-      const city = document.querySelector(".weather span:nth-child(3)");
-      weather.innerText = `날씨: ${data.weather[0].main}`;
-      temperature.innerText = `온도: ${data.main.temp}`;
-      city.innerText = data.name;
-    });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const icon = document.querySelector("#weather-icon");
+        const weather = document.querySelector(".weather span:nth-child(1)");
+        const temperature = document.querySelector(".weather span:nth-child(2)")
+        const city = document.querySelector(".weather span:nth-child(3)");
+
+        icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="날씨 아이콘" id="wn-icon">`;
+        weather.innerHTML = `${data.weather[0].main}`;
+        temperature.innerHTML = `${data.main.temp}°C`;
+        city.innerHTML = data.name;
+      });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
