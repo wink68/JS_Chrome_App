361bdba2ba7735f7204290dbd6033fc24fea3016
김창민 <jinttack@naver.com>
2023-05-07 21:37:11 +0900

    feat: 배경색 변경 버튼

--- js/background.js
+++ js/background.js
@@ -1,4 +1,29 @@
-const images = ["bg1.jpg", "bg2.jpg"];
-const chosenImage = Math.floor(Math.random() * images.length);
+const chosenImage = Math.round(Math.random() * 4);
+const redBtn = document.querySelector("#bgBtnOne");
+const yellowBtn = document.querySelector("#bgBtnTwo");
+const greenBtn = document.querySelector("#bgBtnThree");
+const blueBtn = document.querySelector("#bgBtnFour");
+const blackBtn = document.querySelector("#bgBtnFive");
 
-document.body.style.backgroundImage = `url("img/${images[chosenImage]}")`;
+function handleRedBg() {
+    document.body.style.backgroundImage = `url("img/bg0.png")`;
+}
+function handleYellowBg() {
+    document.body.style.backgroundImage = `url("img/bg1.png")`;
+}
+function handleGreenBg() {
+    document.body.style.backgroundImage = `url("img/bg2.png")`;
+}
+function handleBlueBg() {
+    document.body.style.backgroundImage = `url("img/bg3.png")`;
+}
+function handleBlackBg() {
+    document.body.style.backgroundImage = `url("img/bg4.png")`;
+}
+
+redBtn.addEventListener("click", handleRedBg);
+yellowBtn.addEventListener("click", handleYellowBg);
+greenBtn.addEventListener("click", handleGreenBg);
+blueBtn.addEventListener("click", handleBlueBg);
+blackBtn.addEventListener("click", handleBlackBg);
+document.body.style.backgroundImage = `url("img/bg${chosenImage}.png")`;
