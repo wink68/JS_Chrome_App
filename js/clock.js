171bdc75a9d0cac89ff52053ba2cae92fa94148a
김창민 <jinttack@naver.com>
2023-05-08 12:43:39 +0900

    feat: 날짜

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,6 @@
 const clock = document.querySelector(".clock");
+const today = document.querySelector(".today");
+const weekList = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
 function getClock() {
   const date = new Date();
@@ -8,5 +10,14 @@ function getClock() {
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+function getDate() {
+  const date = new Date();
+  const month = date.getMonth()+1;
+  const day = date.getDate();
+  const week = weekList[date.getDay()];
+  today.innerHTML = `${month}월 ${day}일 ${week}`;
+}
+
+getDate();
 getClock();
 setInterval(getClock, 1000);
